6be5beee0dedc26a76c60cb09b6005b4b0a63016
= <=>
2024-05-15 16:12:42 +0000

    commit 3

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
+<!DOCTYPE  >
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>inscripton</title>
 </head>
 <body>
     <?php
-        require_once './includes/menu.php'
+        require_once '../includes/menu.php';
     ?>
 </body>
 </html>
